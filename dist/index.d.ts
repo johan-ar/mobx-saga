93347e5f5e4966a4c13e4f1ca307c477b8079cb5
johan-ar <jsagudelo@pm.me>
2022-01-21 19:46:20 -0500

    updated

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,16 +3,14 @@ import { MulticastChannel, SagaMonitor } from "@redux-saga/core";
 import { Saga, Task } from "@redux-saga/types";
 import { PutEffect } from "redux-saga/effects";
 export type NonUndefined<T> = Exclude<T, undefined | null | void>;
-export type ActionExtension<O> = O extends void ? {} : A.Compute<Omit<O, "type" | "payload">, "flat">;
-export type PayloadAction<P = void, T extends string = string, Ex extends {
-    [k: string]: any;
-} = never> = [Ex] extends [never] ? {
+export type PayloadAction<P = void, T extends string = string, M = never, E = never> = {
     type: T;
     payload: P;
-} : {
-    type: T;
-    payload: P;
-} & ActionExtension<Ex>;
+} & ([M] extends [never] ? {} : {
+    meta: M;
+}) & ([E] extends [never] ? {} : {
+    error: E;
+});
 export interface Action<T extends string = string> {
     type: T;
 }
@@ -32,13 +30,21 @@ export interface ActionCreatorWithOptionalPayload<P, T extends string = string> 
 export interface ActionCreatorWithPayload<P, T extends string = string> extends BaseActionCreator<P, T> {
     (payload: P): PayloadAction<P, T>;
 }
-export interface ActionCreatorWithPreparedPayload<PA extends PrepareAction<any>, T extends string> extends BaseActionCreator<ReturnType<PA>["payload"], T> {
-    (...args: Parameters<PA>): PayloadAction<ReturnType<PA>["payload"], T, ReturnType<PA>>;
+export type ActionCreatorWithPreparedPayloadHelper<PA extends PrepareAction<any>, T extends string> = ActionCreatorWithPreparedPayload<ReturnType<PA> extends {
+    payload: infer P;
+} ? P : never, T, Parameters<PA>, ReturnType<PA> extends {
+    meta: infer M;
+} ? M : never, ReturnType<PA> extends {
+    error: infer E;
+} ? E : never>;
+export interface ActionCreatorWithPreparedPayload<P, T extends string = string, Args extends any[] = any[], M = never, E = never> extends BaseActionCreator<P, T> {
+    (...args: Args): PayloadAction<P, T, M, E>;
 }
-export type ActionCreator<P = undefined, T extends string = string, PA extends PrepareAction<any> | void = void> = PA extends PrepareAction<any> ? ActionCreatorWithPreparedPayload<PA, T> : A.Is<P, undefined, 'equals'> extends 1 ? ActionCreatorWithoutPayload<T> : A.Is<P, undefined, '<-contains'> extends 1 ? ActionCreatorWithOptionalPayload<NonUndefined<P>, T> : ActionCreatorWithPayload<P, T>;
-type PrepareAction<P> = (...args: any[]) => {
+export type ActionCreator<P = undefined, T extends string = string, PA extends PrepareAction<any> | void = void> = PA extends PrepareAction<any> ? ActionCreatorWithPreparedPayloadHelper<PA, T> : A.Is<P, undefined, 'equals'> extends 1 ? ActionCreatorWithoutPayload<T> : A.Is<P, undefined, '<-contains'> extends 1 ? ActionCreatorWithOptionalPayload<NonUndefined<P>, T> : ActionCreatorWithPayload<P, T>;
+export type PrepareAction<P> = (...args: any[]) => {
     payload: P;
-    [K: string | symbol | number]: any;
+    meta?: any;
+    error?: any;
 };
 export function createAction<P = undefined, T extends string = string>(type: T): ActionCreator<P, T, void>;
 export function createAction<PA extends PrepareAction<any>, T extends string = string>(type: T, prepareAction: PA): ActionCreator<ReturnType<PA>["payload"], T, PA>;
